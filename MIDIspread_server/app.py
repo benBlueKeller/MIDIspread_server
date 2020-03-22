c91f813b3b0db6dcb5681b5cb2b5c198a3dfda3f
benBlueKeller <benblue22@gmail.com>
2020-03-22 02:29:37 -0700

    intial commit

---
+++ MIDIspread_server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, send_file
+from MIDIspread.convert import Convert
+import os
+
+app = Flask(__name__.split(".")[0])
+
+path_mid = os.path.dirname(os.path.abspath(__file__)) + "/midi.mid"
+
+
+@app.route("/", methods=["POST", "PUT"])
+def return_midi():
+    try:
+        if request.method == "POST" or "PUT":
+            params = request.get_json()
+            converter = Convert(**params)
+            converter.data_to_file()
+        return send_file(
+            path_mid,
+            mimetype="audio/midi",
+            attachment_filename=path_mid.split("/")[-1],
+            as_attachment=True,
+        )
+    except Exception as e:
+        raise e
